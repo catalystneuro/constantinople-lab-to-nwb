970922d836f52e186dd56f3347fe628371f14171
weiglszonja <wszonja@gmail.com>
2024-08-30 14:14:54 +0200

    propagate subject metadata

--- src/constantinople_lab_to_nwb/mah_2024/mah_2024_convert_all_sessions.py
+++ src/constantinople_lab_to_nwb/mah_2024/mah_2024_convert_all_sessions.py
@@ -5,12 +5,15 @@ from typing import Union, List
 
 from pymatreader import read_mat
 
-from constantinople_lab_to_nwb.mah_2024.mah_2024_convert_session import session_to_nwb
+from constantinople_lab_to_nwb.mah_2024.mah_2024_convert_session import (
+    session_to_nwb,
+    get_subject_metadata_from_rat_info_folder,
+)
 
 
 def _get_sessions_to_convert_from_mat(
-        file_path: Union[str, Path],
-        default_struct_name: str = "A",
+    file_path: Union[str, Path],
+    default_struct_name: str = "A",
 ) -> List[str]:
     """
     Get the list of sessions to convert from a .mat file.
@@ -37,12 +40,13 @@ def _get_sessions_to_convert_from_mat(
 
 
 def sessions_to_nwb(
-        raw_behavior_folder_path: Union[str, Path],
-        processed_behavior_folder_path: Union[str, Path],
-        nwbfile_folder_path: Union[str, Path],
-        column_name_mapping: dict = None,
-        column_descriptions: dict = None,
-        overwrite: bool = False,
+    raw_behavior_folder_path: Union[str, Path],
+    processed_behavior_folder_path: Union[str, Path],
+    rat_info_folder_path: Union[str, Path],
+    nwbfile_folder_path: Union[str, Path],
+    column_name_mapping: dict = None,
+    column_descriptions: dict = None,
+    overwrite: bool = False,
 ):
     """
     Convert all sessions to NWB format.
@@ -55,6 +59,8 @@ def sessions_to_nwb(
         The path to the folder containing the raw Bpod output files.
     processed_behavior_folder_path: str or Path
         The path to the folder containing the processed behavior files.
+    rat_info_folder_path: str or Path
+        The path to the folder containing the rat info files.
     nwbfile_folder_path: str or Path
         The path to the folder where the NWB files will be saved.
     column_name_mapping: dict, optional
@@ -68,20 +74,26 @@ def sessions_to_nwb(
         os.makedirs(nwbfile_folder_path, exist_ok=True)
 
     processed_mat_files = list(processed_behavior_folder_path.glob("*.mat"))
-    subject_ids = [processed_behavior_file_path.stem.split("_")[-1] for processed_behavior_file_path in processed_mat_files]
+    subject_ids = [
+        processed_behavior_file_path.stem.split("_")[-1] for processed_behavior_file_path in processed_mat_files
+    ]
 
     for subject_id, processed_behavior_file_path in zip(subject_ids, processed_mat_files):
-        if subject_id != "C005":
+        if subject_id != "J076":
             continue
         dates_from_mat = _get_sessions_to_convert_from_mat(file_path=processed_behavior_file_path)
 
         for date_from_mat in dates_from_mat:
-            date_obj = datetime.strptime(date_from_mat, '%d-%b-%Y')
-            formatted_date_str = date_obj.strftime('%Y%m%d')
+            date_obj = datetime.strptime(date_from_mat, "%d-%b-%Y")
+            formatted_date_str = date_obj.strftime("%Y%m%d")
 
-            raw_behavior_file_paths = list((raw_behavior_folder_path / subject_id / "DataFiles").glob(f"*{formatted_date_str}*.mat"))
+            raw_behavior_file_paths = list(
+                (raw_behavior_folder_path / subject_id / "DataFiles").glob(f"*{formatted_date_str}*.mat")
+            )
             if len(raw_behavior_file_paths) != 1:
-                raise ValueError(f"Expected to find 1 raw behavior file for date {formatted_date_str}, found {len(raw_behavior_file_paths)}.")
+                raise ValueError(
+                    f"Expected to find 1 raw behavior file for date {formatted_date_str}, found {len(raw_behavior_file_paths)}."
+                )
             raw_behavior_file_path = raw_behavior_file_paths[0]
 
             session_id = raw_behavior_file_path.stem.split("_", maxsplit=1)[1].replace("_", "-")
@@ -91,6 +103,12 @@ def sessions_to_nwb(
                 print(f"Skipping existing NWB file: {nwbfile_path}")
                 continue
 
+            subject_metadata = get_subject_metadata_from_rat_info_folder(
+                folder_path=rat_info_folder_path,
+                subject_id=subject_id,
+                date=date_obj.strftime("%Y-%m-%d"),
+            )
+
             session_to_nwb(
                 raw_behavior_file_path=raw_behavior_file_path,
                 processed_behavior_file_path=processed_behavior_file_path,
@@ -98,6 +116,7 @@ def sessions_to_nwb(
                 nwbfile_path=nwbfile_path,
                 column_name_mapping=column_name_mapping,
                 column_descriptions=column_descriptions,
+                subject_metadata=subject_metadata,
                 overwrite=overwrite,
             )
 
@@ -105,8 +124,9 @@ def sessions_to_nwb(
 if __name__ == "__main__":
 
     # Parameters for conversion
-    processed_behavior_folder_path = Path(r"/Volumes/T9/Constantinople/A_Structs")
+    processed_behavior_folder_path = Path(r"/Volumes/T9/Constantinople/published/A_Structs_Final")
     raw_behavior_folder_path = Path(r"/Volumes/T9/Constantinople/raw_Bpod")
+    rat_info_folder_path = Path(r"/Volumes/T9/Constantinople/Rat_info")
 
     column_name_mapping = dict(
         hits="is_rewarded",
@@ -125,7 +145,7 @@ if __name__ == "__main__":
         rpokedur="duration_of_right_pokes",
         cpokedur="duration_of_center_pokes",
         rt="reaction_time",
-        slrt="side_poke_reaction_time", # side led on = side poke
+        slrt="side_poke_reaction_time",  # side led on = side poke
         ITI="inter_trial_interval",
     )
     # The column descriptions are used to add descriptions to the columns in the processed data. (optional)
@@ -134,8 +154,8 @@ if __name__ == "__main__":
         vios="Whether the subject violated the trial by not maintaining center poke for the time required by 'nose_in_center'.",
         optout="Whether the subject opted out for each trial.",
         wait_time="The wait time for the subject for for each trial in seconds, after removing outliers."
-                  " For hit trials (when reward was delivered) the wait time is equal to the reward delay."
-                  " For opt-out trials, the wait time is equal to the time waited from trial start to opting out.",
+        " For hit trials (when reward was delivered) the wait time is equal to the reward delay."
+        " For opt-out trials, the wait time is equal to the time waited from trial start to opting out.",
         wait_for_cpoke="The time between side port poke and center poke in seconds, includes the time when the subject is consuming the reward.",
         zwait_for_cpoke="The z-scored wait_for_cpoke using all trials.",
         side="The rewarded port (Left or Right) for each trial.",
@@ -154,11 +174,12 @@ if __name__ == "__main__":
 
     nwbfile_folder_path = Path("/Volumes/T9/Constantinople/nwbfiles")
 
-    overwrite = False
+    overwrite = True
 
     sessions_to_nwb(
         raw_behavior_folder_path=raw_behavior_folder_path,
         processed_behavior_folder_path=processed_behavior_folder_path,
+        rat_info_folder_path=rat_info_folder_path,
         nwbfile_folder_path=nwbfile_folder_path,
         column_name_mapping=column_name_mapping,
         column_descriptions=column_descriptions,
