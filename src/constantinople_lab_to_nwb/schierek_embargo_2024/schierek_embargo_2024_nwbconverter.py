6de6df4abc0ca2230bf420c4aee20954a549f803
weiglszonja <wszonja@gmail.com>
2024-09-13 11:23:40 +0200

    update converter

--- src/constantinople_lab_to_nwb/schierek_embargo_2024/schierek_embargo_2024_nwbconverter.py
+++ src/constantinople_lab_to_nwb/schierek_embargo_2024/schierek_embargo_2024_nwbconverter.py
@@ -11,7 +11,8 @@ from neuroconv.datainterfaces import (
 )
 from neuroconv.utils import FilePathType
 from probeinterface import read_probeinterface, Probe
-from pynwb import NWBFile
+
+from constantinople_lab_to_nwb.schierek_embargo_2024.interfaces import SchierekEmbargo2024SortingInterface
 
 
 class SchierekEmbargo2024NWBConverter(NWBConverter):
@@ -21,6 +22,7 @@ class SchierekEmbargo2024NWBConverter(NWBConverter):
         RecordingAP=OpenEphysRecordingInterface,
         RecordingLFP=OpenEphysRecordingInterface,
         PhySorting=PhySortingInterface,
+        ProcessedSorting=SchierekEmbargo2024SortingInterface,
     )
 
     def _set_probe_properties_for_recording_interface(
@@ -52,6 +54,33 @@ class SchierekEmbargo2024NWBConverter(NWBConverter):
                 except ValueError:
                     warn(f"Property '{property_name}' not found in probe contact vector.", UserWarning)
 
+    def _set_electrode_properties_for_recording_interface(self, recording_interface: str):
+        """
+        Set the electrode properties retrieved from the processed sorting interface for the recording interface.
+
+        Parameters
+        ----------
+        recording_interface: str
+            The name of the recording interface.
+        """
+        recording_extractor = self.data_interface_objects[recording_interface].recording_extractor
+        sorting_extractor = self.data_interface_objects["ProcessedSorting"].sorting_extractor
+        electrode_properties = sorting_extractor._electrode_properties
+
+        # TODO confirm channel indices match to channel info from sorting
+        channel_ids = np.array(electrode_properties["rec_channel"]) - 1  # shift to 0-based indexing
+        channel_ids = list(channel_ids)
+        properties_to_add = [property_name for property_name in electrode_properties if property_name != "rec_channel"]
+        for property_name in properties_to_add:
+            property_values = electrode_properties[property_name]
+            missing_value = "unknown" if property_name == "brain_area" else np.nan
+            channel_indices_from_recording = recording_extractor.ids_to_indices()
+            values_to_add = [
+                property_values[channel_ids.index(i)] if i in channel_ids else missing_value
+                for i in channel_indices_from_recording
+            ]
+            recording_extractor.set_property(property_name, values_to_add)
+
     def __init__(
         self,
         source_data: Dict[str, dict],
@@ -62,14 +91,47 @@ class SchierekEmbargo2024NWBConverter(NWBConverter):
 
         super().__init__(source_data=source_data, verbose=verbose)
 
-        if probe_group_file_path is not None:
-            probe_group = read_probeinterface(file=probe_group_file_path)
-            if len(probe_group.probes) == 1:
-                probe = probe_group.probes[0]
-                # Add probe information to the recording interfaces
-                for recording_interface_name in ["RecordingAP", "RecordingLFP"]:
+        for recording_interface_name in ["RecordingAP", "RecordingLFP"]:
+            if "ProcessedSorting" in self.data_interface_objects:
+                self._set_electrode_properties_for_recording_interface(recording_interface_name)
+
+            if probe_group_file_path is not None:
+                probe_group = read_probeinterface(file=probe_group_file_path)
+                if len(probe_group.probes) == 1:
+                    probe = probe_group.probes[0]
+                    # Add probe information to the recording interfaces
                     self._set_probe_properties_for_recording_interface(
                         probe=probe,
                         properties_to_add=probe_properties,
                         recording_interface=recording_interface_name,
                     )
+
+    def get_metadata(self):
+        metadata = super().get_metadata()
+
+        if "Electrodes" not in metadata["Ecephys"]:
+            metadata["Ecephys"]["Electrodes"] = []
+
+        metadata["Ecephys"]["Electrodes"].extend(
+            [
+                dict(
+                    name="channel_depth_um",
+                    description="The distance of the channel from the tip of the neuropixels probe in micrometers.",
+                ),
+                dict(name="distance_from_L1_um", description="The distance from L1 in micrometers."),
+                dict(
+                    name="x",
+                    description="The anterior/posterior neuropixels probe location relative to Bregma in micrometers.",
+                ),
+                dict(
+                    name="y",
+                    description="The medial/lateral neuropixels probe location relative to Bregma in micrometers.",
+                ),
+                dict(
+                    name="z",
+                    description="The dorsal/ventral neuropixels probe location relative to Bregma in micrometers.",
+                ),
+            ]
+        )
+
+        return metadata
