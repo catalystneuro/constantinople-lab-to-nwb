779c1842e5a3df923c6ca41db3342dd6494e2215
weiglszonja <wszonja@gmail.com>
2024-10-05 18:24:15 +0200

    conditional set data interface for fiber photometry based on file format

--- src/constantinople_lab_to_nwb/fiber_photometry/fiber_photometry_nwbconverter.py
+++ src/constantinople_lab_to_nwb/fiber_photometry/fiber_photometry_nwbconverter.py
@@ -1,14 +1,28 @@
+from pathlib import Path
+
 from neuroconv import NWBConverter
 from neuroconv.datainterfaces import DeepLabCutInterface, VideoInterface
 
-from constantinople_lab_to_nwb.fiber_photometry.interfaces import DoricFiberPhotometryInterface
+from constantinople_lab_to_nwb.fiber_photometry.interfaces import (
+    DoricFiberPhotometryInterface,
+    DoricCsvFiberPhotometryInterface,
+)
 
 
 class FiberPhotometryNWBConverter(NWBConverter):
     """Primary conversion class for converting the Fiber photometry dataset from the Constantinople Lab."""
 
     data_interface_classes = dict(
-        FiberPhotometry=DoricFiberPhotometryInterface,
         DeepLabCut=DeepLabCutInterface,
         Video=VideoInterface,
     )
+
+    def __init__(self, source_data: dict[str, dict], verbose: bool = True):
+        """Validate source_data against source_schema and initialize all data interfaces."""
+        fiber_photometry_source_data = source_data["FiberPhotometry"]
+        fiber_photometry_file_path = Path(fiber_photometry_source_data["file_path"])
+        if fiber_photometry_file_path.suffix == ".doric":
+            self.data_interface_classes["FiberPhotometry"] = DoricFiberPhotometryInterface
+        elif fiber_photometry_file_path.suffix == ".csv":
+            self.data_interface_classes["FiberPhotometry"] = DoricCsvFiberPhotometryInterface
+        super().__init__(source_data=source_data, verbose=verbose)
